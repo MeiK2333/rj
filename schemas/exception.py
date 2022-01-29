3ae00e133c1ecde7678f00f9b8619ffbf0f8d711
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-29 14:28:13 +0800

    Delete schemas directory

--- schemas/exception.py
+++
@@ -1,8 +0,0 @@
-from fastapi import Request
-from starlette.responses import JSONResponse
-
-from config.errcode import RJBaseException
-
-
-async def exception_handler(request: Request, exc: RJBaseException):
-    return JSONResponse(status_code=exc.errcode, content={"errmsg": exc.errmsg})
